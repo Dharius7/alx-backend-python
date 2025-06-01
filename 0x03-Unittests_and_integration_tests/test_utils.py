bfe1c9dfd58bf21dfdc403165b37ded13c3c6572
Dharius7 <azametthew@gmail.com>
2025-06-01 13:57:35 +0000

    first commit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -78,6 +78,7 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
+        """Test that GithubOrgClient.org returns correct data."""
         test_payload = {"login": org_name}
         mock_get_json.return_value = test_payload
         client = GithubOrgClient(org_name)
